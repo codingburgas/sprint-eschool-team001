8350bbc9c0e7a97d33b58b8519d78de302f30519
ddzavalishin23 <ddzavalishin23@codingburgas.bg>
2025-03-26 18:53:13 +0300

    Bug fix

--- Main/Main/test.cpp
+++ Main/Main/test.cpp
@@ -5,15 +5,16 @@
 #include <vector>
 using namespace std;
 void test(int diffculty) {
-	int number_of_questions = 25, total_score = 0,min = 1, max = 25,count = 25;
+	int number_of_questions = 25, total_score = 0, min = 1, max = 25, count = 25;
 
 	vector<int> numbers;
-	for (int i = min; i <= max; ++i) {
+	for (int i = 0; i <= max; ++i) {
 		numbers.push_back(i);
-	
-	random_device rd;
-	mt19937 g(rd());
-	shuffle(numbers.begin(), numbers.end(), g);
+
+		random_device rd;
+		mt19937 g(rd());
+		shuffle(numbers.begin(), numbers.end(), g);
+	}
 		for (size_t i = 0; i < number_of_questions; i++) {
 			switch (diffculty) {
 			case 1:
@@ -28,9 +29,21 @@ void test(int diffculty) {
 				if (question(3, numbers[i]) == true) {
 					total_score += 2;
 				}
-				else {
-					cout << "Wrong Answer!";
-				}
 			}
 		}
-}
+	}
+/*
+* max points: 30/30
+
+
+to estimate yourself, divide your points by 5.
+example: 28/5 = 5.6 That still means 6.
+example 2: 16/5 = 3.2. That means 3.
+6 - excellent;
+5- very good;
+4 - good;
+3 - not really well;
+2 - bad;
+
+
+	*/
